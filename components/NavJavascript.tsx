07fd02e896716aa3ef12c08ca151ede6262ef649
krykenz <krakinzkon@gmail.com>
2023-01-07 18:43:36 +0530

    update

--- components/NavJavascript.tsx
+++ components/NavJavascript.tsx
@@ -16,9 +16,9 @@ export default function NavJavascript() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h16M4 18h7"
               />
             </svg>
