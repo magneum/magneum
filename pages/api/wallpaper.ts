13e063cc95673f9343d375a0155ef319e4bb0a0d
magneum <krakinzkon@gmail.com>
2023-01-24 08:38:30 +0530

    wallpaper

--- pages/api/wallpaper.ts
+++ pages/api/wallpaper.ts
@@ -10,7 +10,7 @@ function Wallpaper_Flare(query: any) {
     axios
       .get("https://www.wallpaperflare.com/search?wallpaper=" + query, {
         headers: {
-          accept: "application/json",
+          accept: "*/*",
           "Content-Type": "application/json",
           "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
