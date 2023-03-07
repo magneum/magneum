90672090042864ebe90f5b590ece7ff83d034c4e
magneum <krakinzkon@gmail.com>
2023-03-07 12:41:08 +0530

    updated

--- app/canvas/index.js
+++ app/canvas/index.js
@@ -1,6 +1,2 @@
-import BallCanvas from "./Ball";
-import StarsCanvas from "./Stars";
-import EarthCanvas from "./Earth";
 import ComputersCanvas from "./Computers";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { ComputersCanvas };
