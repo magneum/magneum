b484e93299f0a2928f57c6d7deb8bc1890003b7f
Mågnëum / <krakinzkon@gmail.com>
2023-03-09 20:40:02 +0530

    updated

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
+export default function Page({ children }: { children: React.ReactNode }) {
   return <div className="snap-y snap-mandatory bg-[#1C1A22] h-screen"></div>;
 }
