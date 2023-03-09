78f0d2f70492e3dffaeb8bb68fb245e726855380
Mågnëum / <krakinzkon@gmail.com>
2023-03-09 21:29:04 +0530

    updated
    updated

--- app/lib/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div className="snap-y snap-mandatory bg-[#1C1A22] h-screen"></div>;
-}
--- public/static/script.js
+++
@@ -1,8 +0,0 @@
-let iterations = 0;
-let letters = "ABVDEFGHIJKLMNOPQRSTUVWXYZ";
-document.querySelector("h6").onmouseover = (event) => {
-  event.target.innerText = event.target.innerText
-    .split("")
-    .map((letter) => letters(Math.floor(Math.random() * 26)))
-    .join("");
-};
